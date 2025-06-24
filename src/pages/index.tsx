74ba941956712895b768f629bb54eb4974bef522
Martinien21 <martiniengbemahloue036@gmail.com>
2025-06-24 14:00:53 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import {Inter } from "next/font/google";
+import { Seo } from "@/ui/components/seo";
+
+const inter = Inter({subsets:["latin"]});
+
+export default function Home() {
+  return (
+    <>
+    <Seo title = "Coders Monkeys" description="description..." />
+    <h1>Bonjour a tous</h1>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // analyse tous les fichiers .html et .js dans src/
+  ],
+  theme: {
+    extend: {}, // personnalisation possible ici
+  },
+  plugins: [], // ajouter des plugins ici si besoin
+}
