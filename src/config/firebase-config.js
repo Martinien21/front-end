071e7180a0f899af3f0e30e5940e2aad2bdbbf8d
Martinien21 <martiniengbemahloue036@gmail.com>
2025-06-25 14:54:10 +0100

    Initial commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpM8FSrYXR_WSApjN5XK4cixXeu20Svb8",
+  authDomain: "coders-monkeys-c3281.firebaseapp.com",
+  projectId: "coders-monkeys-c3281",
+  storageBucket: "coders-monkeys-c3281.firebasestorage.app",
+  messagingSenderId: "663327185787",
+  appId: "1:663327185787:web:a4b59336ddcecc658ffbde"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import {Inter } from "next/font/google";
+//import Head from "next/head";
+//import {Inter } from "next/font/google";
 import { Seo } from "@/ui/components/seo";
 
-const inter = Inter({subsets:["latin"]});
+//const inter = Inter({subsets:["latin"]});
 
 export default function Home() {
   return (
